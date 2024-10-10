0d0467faf1b89749e97d33c04d34eec1af4bda7e
Tim Johansson <totimom@live.se>
2024-10-10 23:48:07 +0200

    fix: Windows support and some bugfixes

--- DummyTest/premake5.lua
+++ DummyTest/premake5.lua
@@ -6,4 +6,6 @@ project "DummyTest"
     includedirs{"../DummyLib/include", "../External/googletest/googletest/", "../External/googletest/googletest/include"}
 
     libdirs{targetBuildPath .. "/googletest"}
-    links{"DummyLib", "gtest"}
+
+    -- Although this consoleApp does not link to the "googletest" project per say, it is still dependant on its build to finish
+    links{"DummyLib", "gtest", "googletest"}
--- External/premake5.lua
+++ External/premake5.lua
@@ -1,15 +1,36 @@
 project "googletest"
-    kind "Makefile"
+    kind "Utility"
     targetdir(targetBuildPath .. "/%{prj.name}")
     objdir(objBuildPath .. "/%{prj.name}")
 
-    googletestDirectory = path.getdirectory(_SCRIPT) .. "/googletest"
+    googletestDirectory = "\"" .. path.getdirectory(_SCRIPT) .. "\"" .. "/googletest"
     googletestBuildFolder = "%{prj.objdir}"
 
-    buildcommands {
-        "{MKDIR} " .. googletestBuildFolder,
-        "cmake -S " .. googletestDirectory .. " -B " .. googletestBuildFolder,
-        "cmake --build " .. googletestBuildFolder .. " --config %{cfg.buildcfg}",
-        "{MKDIR} %{prj.targetdir}",
-        "{COPY} " .. googletestBuildFolder .. "/lib/libgtest.a %{prj.targetdir}/libgtest.a"
-    }
+    filter "system:windows"
+        filter "configurations:debug"
+            prebuildcommands{
+                "{MKDIR} " .. googletestBuildFolder,
+                "cmake -S " .. googletestDirectory .. " -B " .. googletestBuildFolder,
+                "cmake --build " .. googletestBuildFolder .. " --config %{cfg.buildcfg}",
+            }
+            postbuildcommands {
+                "{COPY} " .. googletestBuildFolder .. "/lib/Debug/gtest.pdb" .. " %{prj.targetdir}",
+                "{COPY} " .. googletestBuildFolder .. "/lib/Debug/gtest.lib" .. " %{prj.targetdir}"
+            }
+        filter "configurations:release"
+            prebuildcommands{
+                "{MKDIR} " .. googletestBuildFolder,
+                "cmake -S " .. googletestDirectory .. " -B " .. googletestBuildFolder,
+                "cmake --build " .. googletestBuildFolder .. " --config %{cfg.buildcfg}",
+            }
+            postbuildcommands{
+                "{COPY} " .. googletestBuildFolder .. "/lib/Release/gtest.lib" .. " %{prj.targetdir}"
+            }
+
+    filter "system:linux"
+        prebuildcommands{
+            "{MKDIR} " .. googletestBuildFolder,
+            "cmake -S " .. googletestDirectory .. " -B " .. googletestBuildFolder,
+            "cmake --build " .. googletestBuildFolder .. " --config %{cfg.buildcfg}",
+            --"{COPY} " .. googletestBuildFolder .. "/lib/libgtest.a" .. " %{prj.targetdir}/libgtest.a"
+        }
