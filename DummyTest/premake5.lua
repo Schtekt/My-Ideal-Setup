086cabcbe6f59f3137b4fda2433968769d4e3ed0
Tim Johansson <totimom@live.se>
2025-01-21 18:04:47 +0100

    Make DummyTest dependant on googletest project

--- DummyTest/premake5.lua
+++ DummyTest/premake5.lua
@@ -8,4 +8,6 @@ project "DummyTest"
     libdirs{targetBuildPath .. "/External/lib"}
 
     -- Although this consoleApp does not link to the "googletest" project per say, it is still dependant on its build to finish
+    dependson {"googletest"}
+
     links{"DummyLib", "gtest"}
